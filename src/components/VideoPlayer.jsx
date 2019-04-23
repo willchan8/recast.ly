771b914db1a7615b8094833828c10b432ddfb832
willchan8 <wchan1992@gmail.com>
2019-04-23 11:28:09 -0700

    Update the video details of the video that is passed to VideoPlayer

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,11 +2,11 @@
 var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.videos[0].id.videoId}`} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>{props.videos[0].snippet.title}</h3>
-      <div>{props.videos[0].snippet.description}</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
